d824d91633b11423c182ab9ae308b7b3645894b9
Brice Nguefack <nguefy.nguefy@yahoo.fr>
2026-08-11 21:11:23 +0200

    feat(terraform): expose AKS infrastructure outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -22,3 +22,23 @@ output "aks_oidc_issuer_url" {
   description = "OIDC issuer URL of the AKS cluster."
   value       = azurerm_kubernetes_cluster.main.oidc_issuer_url
 }
+
+output "aks_fqdn" {
+  description = "Fully qualified domain name of the AKS cluster."
+  value       = azurerm_kubernetes_cluster.main.fqdn
+}
+
+output "aks_kubelet_identity_object_id" {
+  description = "Object ID of the AKS kubelet managed identity."
+  value       = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
+}
+
+output "vnet_name" {
+  description = "Name of the Azure virtual network."
+  value       = azurerm_virtual_network.main.name
+}
+
+output "aks_subnet_name" {
+  description = "Name of the AKS subnet."
+  value       = azurerm_subnet.aks.name
+}
