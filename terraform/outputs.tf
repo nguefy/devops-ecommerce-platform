d7efed18e5a52a413f94e6f301eab3bd59ecb47b
Brice Nguefack <nguefy.nguefy@yahoo.fr>
2026-08-11 21:47:54 +0200

    feat(terraform): expose network resource IDs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -42,3 +42,13 @@ output "aks_subnet_name" {
   description = "Name of the AKS subnet."
   value       = azurerm_subnet.aks.name
 }
+
+output "vnet_id" {
+  description = "Resource ID of the Azure virtual network."
+  value       = azurerm_virtual_network.main.id
+}
+
+output "aks_subnet_id" {
+  description = "Resource ID of the AKS subnet."
+  value       = azurerm_subnet.aks.id
+}
